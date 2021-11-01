633a1fae92ac0f0fdd51732a13cce82d8a2369f3
JakeLester03 <90791128+JakeLester03@users.noreply.github.com>
2021-11-01 13:43:24 -0500

    Update LA.py

--- LA.py
+++ LA.py
@@ -1,19 +1,19 @@
 #0 Vector Addition
 
 def add_vectors(vector_a:list[float],vector_b:list[float])->list[float]:
-   """
-    Adds the two input vectors.
-    Creates a result vector stored as a list of 0's the same length as the input 
-    then overwrites each element of the result vector with the corresponding
-    element of the sum of the input vectors. Achieves this using a for loop over
-    the indices of result. 
-    Args:
-        vector_a: A vector stored as a list.
-        vector_b: A vector, the same length as vector_a, stored as a list.
-
-    Returns:
-       The sum of the input vectors stored as a list.
-    """ 
+"""
+   Adds the two input vectors.
+   Creates a result vector stored as a list of 0's the same length as the input 
+   then overwrites each element of the result vector with the corresponding
+   element of the sum of the input vectors. Achieves this using a for loop over
+   the indices of result. 
+Args:
+   vector_a: A vector stored as a list.
+   vector_b: A vector, the same length as vector_a, stored as a list.
+   
+Returns:
+   The sum of the input vectors stored as a list.
+""" 
   result:list[float] = [0 for element in vector_a]
   for index in range(len(result)):
     result[index] = vector_a[index] + vector_b[index]
