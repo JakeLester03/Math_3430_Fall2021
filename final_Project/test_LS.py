c10ea9b62099e62ef7d6604072986c3ddbf22642
JakeLester03 <90791128+JakeLester03@users.noreply.github.com>
2021-11-20 23:56:46 -0600

    Add files via upload

---
+++ final_Project/test_LS.py
@@ -0,0 +1,19 @@
+import LA
+import QR
+import LS
+
+def test_back_substitution():
+    matrix_s = [[1, 0, 0], [0, 1, 0], [2, 2, 1]]
+    matrixζ = [[1, 2, 3], [2, 1, 0], [1, 1, 2]]
+    vector_s = [3, 2, 1]
+    vectorζ = [2, 2, 1]
+    assert LS.back_substitution(matrix_s, vector_s) == [1.0, 0.0, 1.0]
+    assert LS.back_substitution(matrixζ, vectorζ) == [-1.5, 1.5, 0.5]
+
+def test_least_squares():
+    matrix_a = [[4,0,0],[5,1,0],[6,2,3]]
+    vector_a = [3,3,6]
+    matrix_b = [[1,0,0],[0,1,0],[2,2,1]]
+    vector_b = [3,4,5]
+    assert LS.least_squares(matrix_a, vector_a) == [-1.0, -1.0, 2.0]
+    assert LS.least_squares(matrix_b, vector_b) == [-7.0, -6.0, 5.0]
