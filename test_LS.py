515ff07922abcd4779784e33d135d00ba02f85f1
JakeLester03 <90791128+JakeLester03@users.noreply.github.com>
2021-11-20 23:42:51 -0600

    Update test_LS.py
    fixed the name of the test function

--- test_LS.py
+++ test_LS.py
@@ -2,7 +2,7 @@ import LA
 import QR
 import LS
 
-def test_back_substitution():
+def test_least_squares():
     matrix_a = [[4,0,0],[5,1,0],[6,2,3]]
     vector_a = vector_a = [3,3,6]
     matrix_b = [[1,0,0],[0,1,0],[2,2,1]]
