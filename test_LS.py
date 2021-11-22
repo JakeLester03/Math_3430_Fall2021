de983d6022adfa5694a9059dafbced4fe345712c
JakeLester03 <90791128+JakeLester03@users.noreply.github.com>
2021-11-22 00:24:10 -0600

    Update test_LS.py
    updated change to least_squares to reflect changes made in QR.py (deep_copy and conjugate_tranpose handeling non_square matricies); updated test_least_squares with proper numbers.

--- test_LS.py
+++ test_LS.py
@@ -12,8 +12,8 @@ def test_back_substitution():
 
 def test_least_squares():
     matrix_a = [[4,0,0],[5,1,0],[6,2,3]]
-    vector_a = vector_a = [3,3,6]
-    matrix_u = [[1,1,1],[3,2,3], [6,2,8]]
-    vector_u = [3,3,1]
+    vector_a = [3,3,6]
+    matrix_u = [[1,1,1,1],[3,2,3,2], [6,2,8,4]]
+    vector_u = [3,-1,5,1]
     assert LS.least_squares(matrix_a, vector_a) == [-1.0, -1.0, 2.0]
-    assert LS.least_squares(matrix_u, vector_u) == [-2.999999999999919, 3.999999999999955, -0.9999999999999928]
+    assert LS.least_squares(matrix_u, vector_u) == [-2.9999999999999982, -1.0877919644084142e-15, 1.0000000000000002]
