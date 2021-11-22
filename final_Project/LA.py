1501776143605194b9f08ee95370a88cd8d8a6d1
JakeLester03 <90791128+JakeLester03@users.noreply.github.com>
2021-11-22 11:14:57 -0600

    Add files via upload
    updated final project folder with properly working test files

---
+++ final_Project/LA.py
@@ -0,0 +1,196 @@
+def add_vectors(vector_a:list[float], vector_b:list[float])->list[float]:
+    '''
+    Adds the two input vectors.
+    Creates a result vector stored as a list of 0's the same length as the input 
+    then overwrites each element of the result vector with the corresponding
+    element of the sum of the input vectors. Achieves this using a for loop over
+    the indices of result. 
+    Args:
+        vector_a: A vector stored as a list.
+        vector_b: A vector, the same length as vector_a, stored as a list.
+
+    Returns:
+        The sum of the input vectors stored as a list.
+    '''
+    result:list[float] = [0 for element in vector_a]
+    for index in range(len(result)):
+        result[index] = vector_a[index] + vector_b[index]
+    return result  
+
+def scalar_vector_mult(vector: list[float], scalar: float) -> list:   
+    '''
+    Multiplys a vector by a scalar. Creates a result vector stored as a list of 0's the same size of the input vector. Then we overwrite 
+    the elements with the corresponding result of the vector element scaled by the scalar. We can achieve this by using a for loop to toggle
+    through the result.
+    Args: 
+        vector: A vector stored as a list
+        scalar: a scalar represented as a float
+    Returns:
+        the product of the vector and the scalar stored as a list.
+    '''
+    result: list[float] = [0 for element in vector]
+    for index in range(len(vector)):
+        result[index]= (vector[index] * scalar)
+    return result
+
+def scalar_matrix_mult(matrix_a:list[float], scalar: float)->list[float]:
+    '''
+    Multiplys a Matrix by a scalar. We can create an empty list called result. A Matrix will be stored as a list of column vectors. Scalar
+    is scalar. By running a for loop over the indecies of the matirx. We can then multiply the corresponding elements of the column 
+    vectors by the scalar by using scalar_vector_mult. We can then append the result to our empty list and return the result.
+    Args:
+        matrix: a list of column vectors
+        scalar: a scalar represented as a float
+    Returns:
+        the product of the matrix and the scalar stored as list of column vectors.
+    '''
+    result:list[float] = []
+    for index in range(len(matrix_a)):
+        result.append(scalar_vector_mult(matrix_a[index], scalar))
+    return result
+
+def matrix_add(matrix_a:list[float], matrix_b:list[float])->list[float]:
+    '''
+    Used to find the sum of two matricies. We can create an empty list called result. Both matricies stored a column vectors. By indexing
+    over the elements in the stored columns of the first matrix, we can then use add_vectors to compute the sum of the corresponding
+    elements in the column vectores. Append the sum to the result which is the desired sum of the two matricies.
+    Args:
+        matrix_a: a matrix stored as a list of column vectors
+        matrix_b: another matrix of the same size as matrix_a.
+    Returns:
+        the sum of the two matricies stored as a list of column vectors.
+    '''
+    result:list[float] = []
+    for index in range(len(matrix_a)):
+        result.append(add_vectors(matrix_a[index], matrix_b[index]))
+    return result
+
+def matrix_vector_mult(matrix:list[float], vector:list[float])->list[float]:
+    '''
+    Used to find the product of a matrix and a vector.
+    We will create a list of 0's equal to the size of the vector. We can then implement a linear combination
+    by multiplying the elements in the column vectors of matrix_a by the corresponding elements in the vectors. Uzing a for loop
+    over the indecies of the matrix, we can then use add_vectors to add the result and the product of scalar_vector_mult
+    until the result is overwritten.
+    Args:
+        matrix_a: a list of column vectors stored as lists
+        vectors: a vector stored as a list
+    Returns:
+        a new matrix overwritten by the product of the elements in matrix_a and the vector stored as a list of column vectors.
+    '''
+    result:list[float] = [0 for element in matrix]
+    for index in range(len(matrix)):
+        result = (add_vectors(result, scalar_vector_mult(matrix[index], vector[index])))
+    return result
+
+def matrix_matrix_mult(matrix_a:list[float], matrix_b:list[float])->list[float]:
+    '''
+    Used to find the multiplication of two matricies. Both matricies stored as lists of column vectors.
+    Initialize result as an empty list. By indexing though the columns in matrix_b we can calculate product of the 
+    two matricies by using the linear combination method 
+    by appending matrix_vector_mult, which will multiply by the coumns in matrix_a, to the result.
+
+    Args: 
+        matrix_a: a list of column vectors stored as lists
+        matrix_b: another matrix stored as a list fof column vectors. The number of columns of matrix_a will be equal to the number of
+        rows matrix_b has.
+
+    Returns: 
+        The product of the two matricies.
+    '''
+    result:list[float] = []
+    for index in range(len(matrix_b)):
+        result.append(matrix_vector_mult(matrix_a, matrix_b[index]))
+    return result
+
+def absolute_value(scalar: complex) -> complex:
+    '''
+    Used to compute both the absolute value of real and complex numbers. We can set the result equal to the real part 
+    of the scalar squared plus the imaginary part of the scalar squared. Then we take the square root of that result
+    to get the absolute value. In the case of real numbers, thre is no imaginary part
+
+    Args:
+        A scalar stored in a computer as either a complex or real number.
+
+    Returns:
+        The absolute value of the complex or real number
+    '''
+    result: complex = ((scalar.real**2) + (scalar.imag**2))
+    result = result**(1/2)
+    return result
+    
+def p_norm(vector: list[float], p: float = 2) -> float:
+    '''
+    Used to find the P-norm of a vector that defaults to 2. We can initialize result as an empty list. We can then
+    append our absolute value functions to the new list and take the absolute value of each element in the vector.
+    In the same process we can take it to the pth power. Return result as the sum of all the elements raised to 
+    the 1/pth power.
+
+    Args:
+        A vector stored as a list.
+        p is a float which defaults to 2.
+
+    Result:
+        returns the p-norm
+    '''
+    result:list[float] = []
+    for index in range(len(vector)):
+        result.append((absolute_value(vector[index])**p))
+    return (sum(result))**(1/p)
+
+def infinity_norm(vector: list[float]) -> float:
+    '''
+    Used to find the infinity norm of a vector stored as a list. We initialize a result as an empty list.
+    Then we append the absolute value of the indices of the input vector to our new list. Then we can return 
+    the greatest value from the list.
+
+    Args:
+        a vector stored as a list 
+
+    Result:
+        returns the infinity norm of the vector list
+    '''
+    result: list[float] = []
+    for index in range(len(vector)):
+        result.append(absolute_value(vector[index]))
+    return max(result)
+
+def boolean_norm(vector: list[float], p: float=2, boolean: bool = False) -> float:
+    '''
+    Used to compute p-norm or infinity norm of a vector. If the boolean is False, it will default to the 
+    p-norm using our p_norm function, where p = 2. If True, it will go on to calculate the infinity norm using the
+    infinity_norm function.
+
+    Args:
+    - a vector stored as a list.
+    - a scalar p which defaults to 2.
+    - a boolean which defaults as False
+
+    Result:
+    The default result is the p-norm. If true, it will result as the infinity norm.
+    '''
+    if boolean == False:
+        return p_norm(vector, p)
+    else:
+        return infinity_norm(vector)
+
+def inner_product(vector_a:list[complex], vector_b:list[complex]) -> complex:
+    
+    '''
+    Used to calculate the inner product of vectors, whether they are complex or real.
+    Creates a result vector stored as a list of 0's the same size of the input vector. Then we overwrite 
+    the elements with the corresponding result of the vector element multiplied by the other vector using a 
+    for loop to iterate through the elements. Then we can take the sum of the result to get the inner product.
+
+    Args:
+        two vectors stored as lists with real or complex elements.
+
+    Result:
+        The inner product of the two vectors.
+    '''
+    
+    result:list[complex] = [0 for elements in vector_a]
+    for index in range(len(vector_a)):
+        result[index]=vector_a[index] * vector_b[index]
+    return sum(result)
+
---
+++ final_Project/test_LS.py
@@ -0,0 +1,19 @@
+import LA
+import QR
+import LS
+
+def test_back_substitution():
+    matrix_s = [[1, 0, 0], [0, 1, 0], [2, 2, 1]]
+    matrixζ = [[1, 2, 3], [2, 1, 0], [1, 1, 2]]
+    vector_s = [3, 2, 1]
+    vectorζ = [2, 2, 1]
+    assert LS.back_substitution(matrix_s, vector_s) == [1.0, 0.0, 1.0]
+    assert LS.back_substitution(matrixζ, vectorζ) == [-1.5, 1.5, 0.5]
+
+def test_least_squares():
+    matrix_a = [[4,0,0],[5,1,0],[6,2,3]]
+    vector_a = [3,3,6]
+    matrix_u = [[1,1,1,1],[3,2,3,2], [6,2,8,4]]
+    vector_u = [3,-1,5,1]
+    assert LS.least_squares(matrix_a, vector_a) == [-1.0, -1.0, 2.0]
+    assert LS.least_squares(matrix_u, vector_u) == [-2.9999999999999982, -1.0877919644084142e-15, 1.0000000000000002]
